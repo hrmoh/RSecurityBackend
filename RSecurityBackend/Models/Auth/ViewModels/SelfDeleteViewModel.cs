8d3a2d8ad2b2dd651fcac9c6bb20f0f524f868d2
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2022-01-01 20:44:28 +0330

    Add project files.

---
+++ RSecurityBackend/Models/Auth/ViewModels/SelfDeleteViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace RSecurityBackend.Models.Auth.ViewModels
+{
+    /// <summary>
+    /// User Self Delete View Model
+    /// </summary>
+    public class SelfDeleteViewModel
+    {
+        /// <summary>
+        /// user password
+        /// </summary>
+        /// <example>
+        /// Test!123
+        /// </example>     
+        public string Password { get; set; }
+
+        /// <summary>
+        ///CallbackUrl
+        /// </summary>
+        public string CallbackUrl { get; set; }
+    }
+}
