05ccf6cc07266ca078ddbd3110ffec0fb2d93b59
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-09-05 17:57:02 +0330

    #2 api/workspace POST api

--- RSecurityBackend/Controllers/WorkspaceControllerBase.cs
+++ RSecurityBackend/Controllers/WorkspaceControllerBase.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RSecurityBackend.Models.Auth.Db;
+using RSecurityBackend.Models.Auth.Memory;
+using RSecurityBackend.Models.Cloud;
+using RSecurityBackend.Models.Cloud.ViewModels;
+using RSecurityBackend.Models.Generic;
 using RSecurityBackend.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +19,29 @@ namespace RSecurityBackend.Controllers
     /// Worspace API
     /// </summary>
     [Produces("application/json")]
-    [Route("api/users")]
+    [Route("api/workspace")]
     public class WorkspaceControllerBase : Controller
     {
+        /// <summary>
+        /// add workspace
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Authorize]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(RWorkspace))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(string))]
+        [ProducesResponseType((int)HttpStatusCode.Forbidden)]
+        public async Task<IActionResult> Post([FromBody] NewWorkspaceModel model)
+        {
+            Guid loggedOnUserId = new Guid(User.Claims.FirstOrDefault(c => c.Type == "UserId").Value);
+            RServiceResult<RWorkspace> result = await _workspaceService.AddWorkpspaceAsync(loggedOnUserId, model.Name, model.Description, model.IsPublic);
+            if (result.Result == null)
+                return BadRequest(result.ExceptionString);
+            return Ok(result.Result);
+        }
+
+
         /// <summary>
         /// workspace service
         /// </summary>
---
+++ RSecurityBackend/Models/Cloud/ViewModels/NewWorkspaceModel.cs
@@ -0,0 +1,23 @@
+﻿namespace RSecurityBackend.Models.Cloud.ViewModels
+{
+    /// <summary>
+    /// new workspace model
+    /// </summary>
+    public class NewWorkspaceModel
+    {
+        /// <summary>
+        /// name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// every user has access to it (Users is ignored)
+        /// </summary>
+        public bool IsPublic { get; set; }
+    }
+}
