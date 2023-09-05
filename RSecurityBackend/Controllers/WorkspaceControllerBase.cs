2a2967341846bd36668b62bf99348e092279f3b3
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-09-05 16:10:39 +0330

    #2 WorkspaceControllerBase

---
+++ RSecurityBackend/Controllers/WorkspaceControllerBase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RSecurityBackend.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSecurityBackend.Controllers
+{
+    /// <summary>
+    /// Worspace API
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/users")]
+    public class WorkspaceControllerBase : Controller
+    {
+        /// <summary>
+        /// workspace service
+        /// </summary>
+        protected readonly IWorkspaceService _workspaceService;
+        
+        /// <summary>
+        /// workspace service
+        /// </summary>
+        /// <param name="workspaceService"></param>
+        public WorkspaceControllerBase(IWorkspaceService workspaceService)
+        {
+            _workspaceService = workspaceService;
+        }
+    }
+}
