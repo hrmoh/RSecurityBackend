e4d5b36c509e07b9661f05c2fdb41af5f0212093
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-09-05 10:37:34 +0330

    #2 WorkspaceService

---
+++ RSecurityBackend/Services/Implementation/WorkspaceService.cs
@@ -0,0 +1,27 @@
+﻿using RSecurityBackend.DbContext;
+using RSecurityBackend.Models.Auth.Db;
+using System;
+
+namespace RSecurityBackend.Services.Implementation
+{
+    /// <summary>
+    /// Workspace service implementation
+    /// </summary>
+    public class WorkspaceService
+    {
+        /// <summary>
+        /// Database Contetxt
+        /// </summary>
+        protected readonly RSecurityDbContext<RAppUser, RAppRole, Guid> _context;
+
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="context"></param>
+        public WorkspaceService(RSecurityDbContext<RAppUser, RAppRole, Guid> context)
+        {
+            _context = context;
+        }
+    }
+}
