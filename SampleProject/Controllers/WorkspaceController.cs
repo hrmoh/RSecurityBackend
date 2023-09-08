02cc29017f5cd98008d342040b6ef233cfd6f9c3
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-09-08 15:36:59 +0330

    #2 api/workspace/roles

--- SampleProject/Controllers/WorkspaceController.cs
+++ SampleProject/Controllers/WorkspaceController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using RSecurityBackend.Controllers;
 using RSecurityBackend.Services;
 
---
+++ SampleProject/Controllers/WorkspaceRoleController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RSecurityBackend.Controllers;
+using RSecurityBackend.Services;
+
+namespace SampleProject.Controllers
+{
+    /// <summary>
+    /// Workspace User roles
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/workspace/roles")]
+    public class WorkspaceRoleController : WorkspaceRoleControllerBase
+    {
+        public WorkspaceRoleController(IWorkspaceRolesService roleService) : base(roleService)
+        {
+        }
+    }
+}
