2b7afa95e89ac22842c67833f22b54000be09a67
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-09-09 16:29:40 +0330

    #2 WorkspaceUserInvitationViewModel

---
+++ RSecurityBackend/Models/Cloud/ViewModels/WorkspaceUserInvitationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using RSecurityBackend.Models.Auth.ViewModels;
+using System;
+
+namespace RSecurityBackend.Models.Cloud.ViewModels
+{
+    /// <summary>
+    /// workspace user invitation
+    /// </summary>
+    public class WorkspaceUserInvitationViewModel
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// User
+        /// </summary>
+        public PublicRAppUser User { get; set; }
+
+        /// <summary>
+        /// Workspace
+        /// </summary>
+        public WorkspaceViewModel Workspace { get; set; }
+    }
+}
