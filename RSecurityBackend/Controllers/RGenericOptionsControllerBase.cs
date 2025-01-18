a6b5a5be2bd352a3aa93caf3588313121446ce19
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-01-18 17:13:38 +0330

    RGenericOption + WorkspaceId

--- RSecurityBackend/Controllers/RGenericOptionsControllerBase.cs
+++ RSecurityBackend/Controllers/RGenericOptionsControllerBase.cs
@@ -36,7 +36,7 @@ namespace RSecurityBackend.Controllers
         }
 
         /// <summary>
-        /// get user level option, Security Warning: every authenticated user could see value of global options, so do not store sensitive data into them
+        /// get user level option
         /// </summary>
         /// <param name="name"></param>
         /// <param name="value"></param>
@@ -91,10 +91,89 @@ namespace RSecurityBackend.Controllers
             return Ok(res.Result);
         }
 
-       /// <summary>
-       /// constructor
-       /// </summary>
-       /// <param name="optionsService"></param>
+        /// <summary>
+        /// get user level option in a workspace, if option value is not found it returns empty string
+        /// </summary>
+        /// <param name="workspace"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpGet("{workspace}/{name}")]
+        [Authorize]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(string))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(string))]
+        public virtual async Task<IActionResult> GetWorkspaceOptionValue(Guid workspace, string name)
+        {
+            Guid loggedOnUserId = new Guid(User.Claims.FirstOrDefault(c => c.Type == "UserId").Value);
+
+            var res = await _optionsService.GetValueAsync(name, loggedOnUserId, workspace);
+            if (!string.IsNullOrEmpty(res.ExceptionString))
+                return BadRequest(res.ExceptionString);
+            return Ok(res.Result);
+        }
+
+        /// <summary>
+        /// get user level option in a workspace
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="workspace"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [HttpPut("{workspace}/{name}")]
+        [Authorize]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(string))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(string))]
+        public virtual async Task<IActionResult> SetWorkspaceOptionValue(Guid workspace, string name, [FromBody] string value)
+        {
+            Guid loggedOnUserId = new Guid(User.Claims.FirstOrDefault(c => c.Type == "UserId").Value);
+            var res = await _optionsService.SetAsync(name, value, loggedOnUserId, workspace);
+            if (!string.IsNullOrEmpty(res.ExceptionString))
+                return BadRequest(res.ExceptionString);
+            return Ok(res.Result);
+        }
+
+        /// <summary>
+        /// get workspace global option value, Security Warning: every authenticated user in a workspace could see value of global options, so do not store sensitive data into them  or you can override this behaviour in your derived class
+        /// </summary>
+        /// <param name="workspace"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+
+        [HttpGet("global/{workspace}/{name}")]
+        [Authorize]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(string))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(string))]
+        public virtual async Task<IActionResult> GetGlobalWorkspaceOptionValue(Guid workspace, string name)
+        {
+            var res = await _optionsService.GetValueAsync(name, null, workspace);
+            if (!string.IsNullOrEmpty(res.ExceptionString))
+                return BadRequest(res.ExceptionString);
+            return Ok(res.Result);
+        }
+
+
+        /// <summary>
+        /// set global option value, Security Warning: every authenticated user could change value of global options, so do not store sensitive data into them or you can override this behaviour in your derived class
+        /// </summary>
+        /// <param name="workspace"></param>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [HttpPut("global/{workspace}/{name}")]
+        [Authorize]
+        [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(string))]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(string))]
+        public virtual async Task<IActionResult> SetGlobalWorkspaceOptionValue(Guid workspace, string name, [FromBody] string value)
+        {
+            var res = await _optionsService.SetAsync(name, value, null, workspace);
+            if (!string.IsNullOrEmpty(res.ExceptionString))
+                return BadRequest(res.ExceptionString);
+            return Ok(res.Result);
+        }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="optionsService"></param>
         public RGenericOptionsControllerBase(IRGenericOptionsService optionsService)
         {
             _optionsService = optionsService;
