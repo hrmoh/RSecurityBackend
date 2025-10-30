0ee0de1674269b116d146ab906c2b52276bd52a4
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2025-10-30 09:22:16 +0330

    1.6.1

---
+++ SampleProject/Migrations/20251030054345_RChangelogLimits.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SampleProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class RChangelogLimits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "RChangeLogs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Operation",
+                table: "RChangeLogs",
+                type: "nvarchar(24)",
+                maxLength: 24,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EntityName",
+                table: "RChangeLogs",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Operation",
+                table: "RChangeLogs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(24)",
+                oldMaxLength: 24,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EntityName",
+                table: "RChangeLogs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "RChangeLogs",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
