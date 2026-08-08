90ca6ff44a83f55227e1ad565311a45d0868d8bb
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2026-08-08 15:14:27 +0330

    banned phone

---
+++ SampleProject/Migrations/20260808114403_BPhone.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SampleProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class BPhone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "BannedEmails",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "BannedEmails");
+        }
+    }
+}
